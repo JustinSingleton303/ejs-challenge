e9005ac0cf905c68017b81fa1a8c31f7fac5bdc6
JustinSingleton <jsingle9@msudenver.edu>
2021-04-25 18:08:17 -0600

    beginning 3 chaleneges

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", (req, res)=>{
+  res.render("home", {homeStartingContent: homeStartingContent});
+});
+
+app.get("/about", (req, res)=>{
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", (req, res)=>{
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 
